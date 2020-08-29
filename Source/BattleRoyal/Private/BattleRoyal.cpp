135ee89187558432d2448ba023982f4183bad596
Choyeon <Choyeon.engr@gmail.com>
2020-08-29 15:55:15 +0900

    Edit: Log

--- Source/BattleRoyal/Private/BattleRoyal.cpp
+++ Source/BattleRoyal/Private/BattleRoyal.cpp
@@ -1,6 +1,6 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #include "BattleRoyal.h"
 #include "Modules/ModuleManager.h"
+
+DEFINE_LOG_CATEGORY(BattleRoyal);
 
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BattleRoyal, "BattleRoyal" );
