48a011a5f5e9dbdb5e3e5bc98998c07cb7c2f6ad
Choyeon <Choyeon.engr@gmail.com>
2020-09-12 17:15:02 +0900

    Feat: Switch input mode to UI mode on death

--- Source/BattleRoyal/Private/BRPlayerController.cpp
+++ Source/BattleRoyal/Private/BRPlayerController.cpp
@@ -1,14 +1,24 @@
 #include "BRPlayerController.h"
 #include "BRGameMode.h"
+#include "Kismet/GameplayStatics.h"
 
 void ABRPlayerController::ClientDeathResult_Implementation(int32 Rank)
 {
+    SetInputMode(UIMode);
+    bShowMouseCursor = true;
     DeathResult(Rank);
 }
 
 void ABRPlayerController::ClientWinnerResult_Implementation()
 {
+    SetInputMode(UIMode);
+    bShowMouseCursor = true;
     WinnerResult();
+}
+
+void ABRPlayerController::ClientGoToLobby_Implementation()
+{
+    UGameplayStatics::OpenLevel(GetWorld(), FName(TEXT("Lobby")), true);
 }
 
 void ABRPlayerController::Dead()
